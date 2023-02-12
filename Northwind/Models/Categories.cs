fafb8f7a5031f676de987176926a8d69c0856b9b
Ivan Sultanov <Ivan_Sultanov@epam.com>
2023-02-12 20:22:16 +0600

    Add validation for category name

--- Northwind/Models/Categories.cs
+++ Northwind/Models/Categories.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Northwind.Models
 {
     public class Categories
     {
         public int CategoryId { get; set; }
+
+        [StringLength(15)]
         public string CategoryName { get; set; }
         public string Description { get; set; }
         public byte[] Picture { get; set; }
